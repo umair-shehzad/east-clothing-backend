cd563aa7b00ca02ba639343a3bf6e4c38e02d31f
Umair Shehzad <umairshehzad247365@gmail.com>
2023-04-08 10:51:00 +0500

    code cleanup

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import morgan from "morgan";
-import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import morgan from 'morgan';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
 
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(cors());
 
-if (process.env.NODE_ENV === "developement") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'developement') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-app.use(`${process.env.REACT_APP_API_URL}/api/products`, productRoutes);
-app.use(`${process.env.REACT_APP_API_URL}/api/users`, userRoutes);
-app.use(`${process.env.REACT_APP_API_URL}/api/orders`, orderRoutes);
-app.get(`${process.env.REACT_APP_API_URL}/api/config/paypal`, (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
+app.use(`/api/products`, productRoutes);
+app.use(`/api/users`, userRoutes);
+app.use(`/api/orders`, orderRoutes);
+app.get(`/api/config/paypal`, (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID),
 );
-
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is Runn....");
-  });
-}
 
 app.use(notFound);
 app.use(errorHandler);
@@ -48,8 +36,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
 );
-
-//runingin
